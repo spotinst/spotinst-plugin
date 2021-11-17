a9ff1cd6c5e4d18e9c344de61be1b9e32a14a80c
lirona <Liron.Arad@netapp.com>
2021-11-17 14:10:42 +0200

    load all aws instance types - first fallback working version

--- src/main/java/hudson/plugins/spotinst/cloud/SpotinstInstanceWeight.java
+++ src/main/java/hudson/plugins/spotinst/cloud/SpotinstInstanceWeight.java
@@ -4,16 +4,17 @@ import hudson.Extension;
 import hudson.model.Describable;
 import hudson.model.Descriptor;
 import hudson.plugins.spotinst.api.infra.ApiResponse;
-import hudson.plugins.spotinst.common.AwsInstanceTypeDynamicEnum;
 import hudson.plugins.spotinst.common.AwsInstanceTypeEnum;
 import hudson.plugins.spotinst.common.SpotinstContext;
 import hudson.plugins.spotinst.model.aws.AwsInstanceType;
 import hudson.plugins.spotinst.repos.IAwsGroupRepo;
 import hudson.plugins.spotinst.repos.RepoManager;
+import hudson.util.ListBoxModel;
 import jenkins.model.Jenkins;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -23,7 +24,7 @@ public class SpotinstInstanceWeight implements Describable<SpotinstInstanceWeigh
     //region Members
     private AwsInstanceTypeEnum awsInstanceType;
     private Integer             executors;
-    List<AwsInstanceType> allInstanceTypes;
+    private static String       awsInstanceTypeFromAPI;
     //endregion
 
     //region Constructors
@@ -50,26 +51,58 @@ public class SpotinstInstanceWeight implements Describable<SpotinstInstanceWeigh
     //region Private Methods
 
     @DataBoundSetter
-    private void setAllInstanceTypes() {
-        String token     = SpotinstContext.getInstance().getSpotinstToken();
-        String accountId = SpotinstContext.getInstance().getAccountId();
+    public void setAwsInstanceTypeFromAPI(String awsInstanceTypeFromAPI) {
+        this.awsInstanceTypeFromAPI = awsInstanceTypeFromAPI;
+    }
 
-        if (token != null && accountId != null) {
-            IAwsGroupRepo                      awsGroupRepo             = RepoManager.getInstance().getAwsGroupRepo();
-            ApiResponse<List<AwsInstanceType>> allInstanceTypesResponse = awsGroupRepo.getAllInstanceTypes(accountId);
-            this.allInstanceTypes = allInstanceTypesResponse.getValue();
-            AwsInstanceTypeDynamicEnum.setValues(this.allInstanceTypes);
+    private static List<AwsInstanceType> loadAllInstanceTypes() {
+        List<AwsInstanceType>              retVal                   = new ArrayList<>();
+        String                             accountId                = SpotinstContext.getInstance().getAccountId();
+        IAwsGroupRepo                      awsGroupRepo             = RepoManager.getInstance().getAwsGroupRepo();
+        ApiResponse<List<AwsInstanceType>> allInstanceTypesResponse = awsGroupRepo.getAllInstanceTypes(accountId);
+        Boolean isRequestSucceed = allInstanceTypesResponse.isRequestSucceed();
+
+        if (isRequestSucceed) {
+            retVal = allInstanceTypesResponse.getValue();
         }
+        else {
+            for (AwsInstanceTypeEnum instanceTypeEnum : AwsInstanceTypeEnum.values()) {
+                String          type         = instanceTypeEnum.getValue();
+                Integer         cpus         = instanceTypeEnum.getExecutors();
+                AwsInstanceType instanceType = new AwsInstanceType();
+                instanceType.setInstanceType(type);
+                instanceType.setCpus(cpus);
+                retVal.add(instanceType);
+            }
+        }
+
+        return retVal;
     }
     //endregion
 
     //region Classes
     @Extension
     public static final class DescriptorImpl extends Descriptor<SpotinstInstanceWeight> {
+        public  DescriptorImpl(){
+            load();
+        }
 
         @Override
         public String getDisplayName() {
             return "Spot Instance Weight";
+        }
+
+        public ListBoxModel doFillAwsInstanceTypeFromAPIItems() {
+            ListBoxModel retVal = new ListBoxModel();
+            List<AwsInstanceType> allInstanceTypes = loadAllInstanceTypes();
+
+            if(allInstanceTypes != null) {
+                for (AwsInstanceType instanceType : allInstanceTypes) {
+                    retVal.add(instanceType.getInstanceType());
+                }
+            }
+
+            return retVal;
         }
     }
     //endregion
